cf5493a755f07a2cce63fe9fbb4b9616e31181b5
xxxxfault <jiaqian0730@gmail.com>
2022-11-06 10:55:28 -0500

    A7

--- src/tuiter/profile/index.js
+++ src/tuiter/profile/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const ProfileComponent = (
 
